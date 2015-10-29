d9bd77ffae2f7de62304521ed6062f33d5f44742
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-10-29 12:58:13 +0100

    add print in terminal

--- challenge_final.rb
+++ challenge_final.rb
@@ -5,5 +5,15 @@ puts 'Saisir un texte :'
 text = gets.chomp
 
 qr = RQRCode::QRCode.new( text, :size => 4, :level => :h )
+
+# save the QRcode
 png = qr.to_img
 png.resize(90, 90).save("text.png")
+
+# print the QRcode in the terminal
+qr.modules.each do |row|
+	row.each do |col| 
+		print col ? "X" : " "
+		end
+	print "\n"
+end
